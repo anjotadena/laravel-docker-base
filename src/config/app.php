996171b3f00a91ab6738370657b27d0b79b800e6
Anjo Tadena <tadena.anjo@gmail.com>
2022-02-22 01:09:06 +0800

    Set app version

--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,17 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
-
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when
+    | the framework needs to place the application's version in a notification
+    | or any other location as required by the application or its packages.
+    */
+    'version' => env('APP_VERSION', '1.0.0'),
+    
     /*
     |--------------------------------------------------------------------------
     | Application Name
