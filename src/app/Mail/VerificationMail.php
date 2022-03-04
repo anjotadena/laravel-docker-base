555d4c4f77493fafed6f4d5f8bafaaaa8dc04b01
Anjo Tadena <tadena.anjo@gmail.com>
2022-03-04 23:50:21 +0800

    Add user email verification function

---
+++ src/app/Mail/VerificationMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'test@mailinator.com';
+        $subject = 'Test verication email';
+        $name = 'Jane Doe';
+
+        return $this->view('emails.verification')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject)
+                    ->with(['message' => $this->data['message']]);
+    }
+}
---
+++ src/resources/views/emails/verification.blade.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html lang=”en-US”>
+    <head>
+        <meta charset=”utf-8">
+    </head>
+    <body>
+        <h2>Test Email</h2>
+        <p>{{ $message }}</p>
+    </body>
+</html>
