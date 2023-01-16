f728264e4cc46a70bab087f871241cd7ad54616d
Anjo Tadena <tadena.anjo@gmail.com>
2023-01-17 00:45:19 +0800

    Remove not relevant code

--- src/resources/js/components/App.tsx
+++ src/resources/js/components/App.tsx
@@ -1,34 +1,9 @@
-import React, { useState } from "react";
-
-export interface IUser {
-    name: string;
-    age: number;
-}
+import React from "react";
 
 const App = () => {
-    const [users, setUsers] = useState<IUser[]>([
-        {
-            name: "Bijaya",
-            age: 25,
-        },
-        {
-            name: "Ram",
-            age: 25,
-        },
-    ]);
-
     return (
         <div>
-            <h1>Users list</h1>
-            <ul>
-                {users.map((user: IUser) => {
-                    return (
-                        <li key={user.name}>
-                            {user.name} is {user.age} years old
-                        </li>
-                    );
-                })}
-            </ul>
+            <h1>Hello from react typescript!</h1>
         </div>
     );
 };
